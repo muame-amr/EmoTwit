ef80234a15abf57639c38d216d08c283d9d67ea9
Muhammad Amiruddin <m.amiruddin27@gmail.com>
2022-04-25 09:11:42 +0800

    put variable to use

--- webapp/pages/api/sentiment.js
+++ webapp/pages/api/sentiment.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const baseURL = "http://localhost:8080/api/";
 
 export const getTweet = () => {
-	return fetch("http://localhost:8080/api/view").then((res) => res.json());
+	return fetch(`${baseURL}view`).then((res) => res.json());
 };
 
 export const searchTweet = async (keyword) => {
